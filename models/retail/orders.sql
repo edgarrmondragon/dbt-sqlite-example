a914352d23aec3202396dadcca760df0cfd921f3
Edgar Ramírez <edgarrm358@gmail.com>
2021-07-09 01:35:56 -0500

    Use nested credentials (#2)
    * Update source definition
    
    * Use nested SQLite config
    
    * Document models
    
    * Use pyproject.toml for SQLFluff rules
    
    * Exclude rule L005
    
    * Use official dbt-sqlite 0.1.2
    
    * Update README

--- models/retail/orders.sql
+++ models/retail/orders.sql
@@ -5,7 +5,7 @@ customer_cohorts as (
   select
     customer_id
     , date(min(date), 'start of month') as cohort_month
-  from {{ source('etl', 'scanner_data') }}
+  from {{ source('retail', 'scanner_data') }}
   group by 1
 
 )
@@ -19,7 +19,7 @@ customer_cohorts as (
     , count(sku) as items
     , count(distinct sku) as unique_items
     , sum(sales_amount) as amount
-  from {{ source('etl', 'scanner_data') }}
+  from {{ source('retail', 'scanner_data') }}
   group by 1
 
 )
