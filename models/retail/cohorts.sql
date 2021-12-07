74ffdf1fb352ec3787abd06c0a63cc5e03a3ea84
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-12-07 01:46:00 -0600

    style: fix new lines

--- models/retail/cohorts.sql
+++ models/retail/cohorts.sql
@@ -4,7 +4,7 @@ select
   , cast(
     round(sum(case when cohort_index = {{ i }} then amount else 0 end), 2) as real
   ) as index_{{i}}
-  {%- endfor %}
+  {% endfor -%}
   , cast(sum(amount) as real) as overall
 from {{ ref('orders') }}
 group by cohort_month
