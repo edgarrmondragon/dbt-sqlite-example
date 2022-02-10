b7269a304316ab98552963e016244f16e8ce0a7e
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-02-10 11:59:35 -0600

    fix: rule L046 for jinja tags

--- models/retail/cohorts.sql
+++ models/retail/cohorts.sql
@@ -3,7 +3,7 @@ select
   {% for i in range(15) -%}
   , cast(
     round(sum(case when cohort_index = {{ i }} then amount else 0 end), 2) as real
-  ) as index_{{i}}
+  ) as index_{{ i }}
   {% endfor -%}
   , cast(sum(amount) as real) as overall
 from {{ ref('orders') }}
